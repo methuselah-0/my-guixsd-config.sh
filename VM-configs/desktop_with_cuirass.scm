7253d48bda70f628d99d5137da4e0eeec2397ab6
methuselah-0 <david.larsson@selfhosted.xyz>
2020-11-03 15:12:21 +0100

    minor cleanups

--- VM-configs/desktop_with_cuirass.scm
+++ VM-configs/desktop_with_cuirass.scm
@@ -1,7 +1,8 @@
 ;; This is an operating system configuration template
 ;; for a "bare bones" setup, with no X11 display server.
 
-(use-modules (gnu) (gnu system nss))
+;; added gnu packages only because of complaints in /var/log/cuirass/evaluations/1.gz
+(use-modules (gnu) (gnu system nss) (gnu packages))
 ;; additions: spice, cuirass, virtualization (qemu-binfmt-service-type)
 (use-service-modules networking ssh desktop spice cuirass virtualization)
 ;; additions: bash
@@ -14,44 +15,7 @@
     ;;    ("/usr/bin/perl" ,(file-append perl "/bin/perl"))
     ("/usr/bin/env" ,(file-append coreutils "/bin/env"))))
 
-;; to try new configs here, run: herd stop cuirass-web ; herd stop cuirass ; rm /var/lib/cuirass/cuirass.db* ; guix system reconfigure /home/user1/src/my-guixsd-config.sh/VM-configs/bare-bones2.scm ;  herd start cuirass ; sleep 1 ; herd restart cuirass-web
-
-;; old: herd stop cuirass-web ; herd stop cuirass ; rm /var/lib/cuirass/cuirass.db* ; guix system reconfigure -L /home/user1/src/my-guixsd-config.sh/VM-configs desktop_vm.scm ; herd stop ovpn-client ; herd restart cuirass ; sleep 1 ; herd restart cuirass-web
-(define %spec-one
-  '((#:name . "my-other-pkgs")
-    (#:load-path-inputs . ("guix"))
-    ;;(#:package-path-inputs . ("my-guixsd-config.sh"))
-    (#:package-path-inputs . ("my-guix-packages"))
-    ;;(#:package-path-inputs . ())
-    (#:proc-input . "guix")
-    (#:proc-file . "build-aux/cuirass/gnu-system.scm")
-    (#:proc . cuirass-jobs)
-    (#:proc-args .
-     ((subset . ("perl-ical-data" "perl-moose"))
-      (systems . ("x86_64-linux"))))
-    (#:inputs . (
-                    ((#:name . "guix")
-                     (#:url . "git://git.savannah.gnu.org/guix.git")
-                     (#:load-path . ".")
-                     ;;(#:branch . "master")
-                     (#:commit . "697d2e037bc999760b85b7904079679e0ccc1e62")
-                     (#:no-compile? . #t))
-                    
-                    ((#:name . "my-guix-packages")
-                     (#:url . "https://github.com/methuselah-0/my-guix-packages.git")
-                     (#:load-path . "packages")
-                     (#:branch . "master")
-                     (#:no-compile? . #t))                    
-                    ))
-    (#:build-outputs . ())))
-(define %spec-two
-)
-
-                    ;; ((#:name . "bcu")
-                    ;;  (#:url . "https://gitlab.com/methuselah-0/bash-coding-utils.sh.git")
-                    ;;  (#:load-path . "guix-channel")
-                    ;;  (#:branch . "master")
-                    ;;  (#:no-compile? . #t))
+;; to try new configs here, run:  rm -rf /home/cuirass/{my-guixsd-config.sh,my-guix-packages} ; cp -a /home/user1/src/{my-guix-packages,my-guixsd-config.sh} /home/cuirass/ ; chown -R cuirass:cuirass /home/cuirass/{my-guix-packages,my-guixsd-config.sh}  ; herd stop cuirass-web ; herd stop cuirass ; rm /var/lib/cuirass/cuirass.db* ; guix system reconfigure --allow-downgrades /home/user1/src/my-guixsd-config.sh/VM-configs/desktop_with_cuirass.scm ;  herd start cuirass ; sleep 1 ; herd restart cuirass-web ; tail -f /var/log/cuirass.log
 
                     ;; (((#:name . "divoplade-site")
                     ;;  (#:url . "file:///tmp/divoplade-site.git")
@@ -59,50 +23,52 @@
                     ;;  (#:branch . "master")
                     ;;  (#:no-compile? . #t))
 
+;; NOTE: if running this cuirass-specs with a /root/.config/guix/current somewhere after 594b2a1 cuirass-web will not report success vs failure etc. properly see bug-guix mail archive for around oct/nov 2020.
+;; NOTE2: packages must have a proper license to not fail hard - strings or #f will cause a hard fail.
 (define %cuirass-specs
   #~(
      ;; spec two
      list
-  '((#:name . "my-pkgs")
-    (#:load-path-inputs . ("guix"))
-    ;;(#:package-path-inputs . ("my-guixsd-config.sh"))
-    (#:package-path-inputs . ("my-guix-packages"))
-    ;;(#:package-path-inputs . ())
-    ;;(#:package-path-inputs . ())
-    (#:proc-input . "guix")
-    (#:proc-file . "build-aux/cuirass/gnu-system.scm")
-    (#:proc . cuirass-jobs)
-    (#:proc-args .
-     (;;(subset . manifests)
-      ;;(subset . ("hello" "cowsay" "perl-moose" "jupyter" "enlightenment"))
-      (subset . ("hello" "cowsay" "orgmk" "fwknop"))
-      (systems . ("x86_64-linux"))
-      ;;(manifests . (("my-guix-packages" . "manifest.scm")))
-      ))
-    (#:inputs . (
-                 ((#:name . "guix")
-                  (#:url . "git://git.savannah.gnu.org/guix.git")
-                  (#:load-path . ".")
-                  ;;(#:branch . "master")
-                  (#:commit . "d7e033b9a153a9e60f52ff64f4eb355c1c3d0a6e")
-                  (#:no-compile? . #t))
-                 ;; still needs to be a git repo
-                 ;; ((#:name . "my-guix-packages")
-                 ;;  (#:url . "file:///home/cuirass/my-guix-packages")
-                 ;;  (#:load-path . "packages")
-                 ;;  (#:branch . "master")
-                 ;;  (#:no-compile? . #t))                    
-
-                 ((#:name . "my-guix-packages")
-                  (#:url . "https://github.com/methuselah-0/my-guix-packages.git")
-                  (#:load-path . "packages")
-                  (#:branch . "master")
-                  (#:no-compile? . #t))                    
-
-                 ))
-    (#:build-outputs . ()))     
-     
-))
+     '((#:name . "my-pkgs")
+       (#:load-path-inputs . ("guix"))
+       (#:package-path-inputs . ("my-guix-packages"))
+       (#:proc-input . "guix")
+       (#:proc-file . "build-aux/cuirass/gnu-system.scm")
+       (#:proc . cuirass-jobs)
+       (#:proc-args .
+        ((subset . "manifests")
+         (systems . ("x86_64-linux"))
+         (manifests . (("config" . "manifests/user1.scm")))
+         ))
+       (#:inputs . (
+                    ((#:name . "guix")
+                     (#:url . "git://git.savannah.gnu.org/guix.git")
+                     (#:load-path . ".")
+                     (#:branch . "master")
+                     ;;(#:commit . "21df702808a8f4491e1ef5badc607748288fa69a")
+                     ;; 
+                     ;; d7 commit reports failed builds in /var/log/cuirass.log and cuirass-web reports them as scheduled.
+                     ;;(#:commit . "d7e033b9a153a9e60f52ff64f4eb355c1c3d0a6e")
+                  
+                     ;; 594b commit works - success - until adding say perl, python or bash-coding-utils.sh package from my-guix-packages
+                     ;;(#:commit . "594b2a116ea4267d88a294dd05f8dbbb8ce7bcc0")
+                     (#:no-compile? . #t))
+                    ;;still needs to be a git repo
+                    ((#:name . "my-guix-packages")
+                     ;;(#:url . "file:///home/cuirass/my-guix-packages")
+                     (#:url . "https://github.com/methuselah-0/my-guix-packages.git")
+                     (#:load-path . "packages")
+                     (#:branch . "master")
+                     (#:no-compile? . #t))
+                 
+                    ((#:name . "config")
+                     ;;(#:url . "file:///home/cuirass/my-guixsd-config.sh")
+                     (#:url . "https://github.com/methuselah-0/my-guixsd-config.sh.git")
+                     (#:load-path . ".")
+                     (#:branch . "master")
+                     (#:no-compile? . #t))
+                    ))
+       (#:build-outputs . ()))))
 
 (operating-system
   (host-name "librem13v3guixsd")
@@ -135,6 +101,17 @@
                  (supplementary-groups '("wheel"
                                          "audio" "video"))
                  (home-directory "/home/user1"))
+                (user-account
+                 (name "user2")
+                 (group "users")
+                 
+                 ;; Adding the account to the "wheel" group
+                 ;; makes it a sudoer.  Adding it to "audio"
+                 ;; and "video" allows the user to play sound
+                 ;; and access the webcam.
+                 (supplementary-groups '("wheel"
+                                         "audio" "video"))
+                 (home-directory "/home/user2"))                
                ;; openvpn user not needed here
                ;; (user-account
                ;;  (name "openvpn")
@@ -184,7 +161,7 @@
                     ;; https://lists.gnu.org/archive/html/help-guix/2019-06/msg00116.html
                     (service cuirass-service-type
                              (cuirass-configuration
-                              (interval 3600)
+                              (interval 60) ;; 10 hours
                               (fallback? #f) ;; default is #f
                               (host "0.0.0.0")
                               (port 8082)
